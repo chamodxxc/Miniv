eb73362e115d3323ead659b61c2cecb5da347c1a
chamodxxc <nimsarawanas.inhe@gmail.com>
2025-12-11 00:48:19 +0530

    Update config.js

--- config.js
+++ config.js
@@ -14,7 +14,7 @@ function convertToBool(text, fault = 'true') {
 
 module.exports = {
     LANG: 'si',
-    WELCOME: 'false,
+    WELCOME: 'false',
     AUTO_VIEW_STATUS: 'true',
     AUTO_VOICE: 'true',
     AUTO_LIKE_STATUS: 'true',
